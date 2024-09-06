b3078a838e69d27666e25aadc9345794b33df6ab
Paola-ramirez128 <a.22308051281284@cbtis128.edu.mx>
2024-09-06 09:32:46 -0600

    Add files via upload
    subí dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,25 @@
+'''paola ramirez
+   trabajando con cadenas
+'''
+
+nombre="Paola"
+print(nombre[0])
+print(nombre)
+for c in "Paola":
+    print(c)
+
+MisNovias=["Agripina","Anastacia","Maria"]
+for chicas in MisNovias:
+    print(chicas)
+
+    print("otro ejemplo")
+    b = "Paola Ramirez"
+print(b[2:5])
+print(" a mayusculas")
+a = "paola ramirez"
+print(a.upper())
+print("Concatenar")
+a = "Maria"
+b = "Perez"
+c = a + " " + b 
+print(c)
